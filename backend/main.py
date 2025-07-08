92037d4f357f26851feee6c8b0886cf6b3f5814f
webtilians <enrikelr@hotmail.com>
2025-07-08 23:56:17 +0200

    2 Python + React

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,13 @@
-# backend/main.py
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def read_root():
     return {"message": "Hola Mundo desde FastAPI!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # lee el puerto que Railway le pasa
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
